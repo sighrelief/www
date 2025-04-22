647d1ff38aa3491b9dd127e10371972396ab2f3f
Andres <andres99.dev@gmail.com>
2025-04-22 05:01:43 +0200

    i'm confused honestly, again and even more than before

--- src/trpc/react.tsx
+++ src/trpc/react.tsx
@@ -57,6 +57,12 @@ export function TRPCReactProvider(props: { children: React.ReactNode }) {
             headers.set('x-trpc-source', 'nextjs-react')
             return headers
           },
+          fetch: (url, options) => {
+            return fetch(url, {
+              ...options,
+              keepalive: true,
+            })
+          },
         }),
       ],
     })
